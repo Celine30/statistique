91c3f68fe85ec85cb49cb2d3d8326b8c11a49a15
Celine <celi.caurier@gmail.com>
2020-04-04 14:42:26 +0200

    Work Progression and finish Table

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 
 function main() {
 
-
 }
 
 
@@ -15,9 +14,8 @@ function fetchAllUser() {
         return response.json()
     })
     .then(json => {
-/*         console.log(json)
-        console.log(json.data.rows.length) */
         add(json.data.rows)
+        stat(json.data.rows)
     })
 }
 
@@ -27,20 +25,97 @@ function add(allState){
 
     for(i=0; i<allState.length; i++){
 
+        let table = document.createElement("tr");
+        document.getElementById("table").appendChild(table)
 
-        let table = document.createElement("th");
-        document.getElementById('sectionCountry').appendChild(table)
-
-        let titleCountry = document.createElement('th')
+        let titleCountry = document.createElement('td')
+        titleCountry.setAttribute("class","d-flex justify-content-between align-items-center")
         titleCountry.textContent = allState[i].country
-        document.getElementById('sectionCountry') .appendChild(titleCountry)
+        table .appendChild(titleCountry)
+
+        let imgFlag = document.createElement('img')
+        imgFlag.setAttribute("class","img_flag ")
+        imgFlag.src = allState[i].flag;
+        titleCountry .appendChild(imgFlag)
 
         let total_cases = document.createElement('td')
         total_cases.textContent = allState[i].total_cases
-        document.getElementById('sectionCountry') .appendChild(total_cases)
+        total_cases.setAttribute("scope","col")
+        table .appendChild(total_cases)
 
-        let new_cases = document.createElement('td')
-        new_cases.textContent = allState[i].new_cases
-        document.getElementById('sectionCountry') .appendChild(new_cases)
-      }
+
+        let total_deaths = document.createElement('td')
+        total_deaths.textContent = allState[i].total_deaths
+        table.appendChild(total_deaths)
+
+        let total_recovered = document.createElement('td')
+        total_recovered.textContent = allState[i].total_recovered
+        table.appendChild(total_recovered)
+
+        let active_cases = document.createElement('td')
+        active_cases.textContent = allState[i].active_cases
+        table.appendChild(active_cases)
+
+        let serious_critical = document.createElement('td')
+        serious_critical.textContent = allState[i].serious_critical
+        table.appendChild(serious_critical)
+    }
+}
+function stat(allState){
+    for(i=0; i<allState.length; i++){
+
+        let pourcentage_total_deaths = (allState[i].total_deaths * allState[i].total_cases /100)
+
+        let statCountry = document.createElement('div')
+        statCountry.setAttribute("class","progress")
+        document.getElementById('blocStat') .appendChild(statCountry)
+
+        let stat_total_cases = document.createElement('div')
+        stat_total_cases.setAttribute("class","progress-bar")
+        stat_total_cases.setAttribute("role","progressbar")
+        stat_total_cases.setAttribute("style","width: 100%")
+        stat_total_cases.setAttribute("aria-valuenow","25")
+        stat_total_cases.setAttribute("aria-valuemin","0")
+        stat_total_cases.setAttribute("aria-valuemax","100")
+        stat_total_cases.textContent = allState[i].total_cases +" - 100 %"
+        statCountry .appendChild(stat_total_cases)
+
+        let statControl = document.createElement('div')
+        statControl.setAttribute("class","progress")
+        document.getElementById('blocStat') .appendChild(statControl) 
+
+        let stat_total_deaths = document.createElement('div')
+        stat_total_deaths.setAttribute("class","progress-bar bg-danger")
+        stat_total_deaths.setAttribute("role","progressbar")
+        stat_total_deaths.setAttribute("style","width: 25%")
+        stat_total_deaths.setAttribute("aria-valuenow","25")
+        stat_total_deaths.setAttribute("aria-valuemin","0")
+        stat_total_deaths.setAttribute("aria-valuemax","100")
+        stat_total_deaths.textContent = allState[i].total_deaths +" - " + pourcentage_total_deaths + " %"
+        statControl .appendChild(stat_total_deaths)
+
+        let stat_total_recovered = document.createElement('div')
+        stat_total_recovered.setAttribute("class","progress-bar bg-warning")
+        stat_total_recovered.setAttribute("role","progressbar")
+        stat_total_recovered.setAttribute("style","width: 25%")
+        stat_total_recovered.setAttribute("aria-valuenow","25")
+        stat_total_recovered.setAttribute("aria-valuemin","0")
+        stat_total_recovered.setAttribute("aria-valuemax","100")
+        stat_total_recovered.textContent = allState[i].total_recovered
+        statControl .appendChild(stat_total_recovered)
+
+        let stat_active_cases = document.createElement('div')
+        stat_active_cases.setAttribute("class","progress-bar bg-success")
+        stat_active_cases.setAttribute("role","progressbar")
+        stat_active_cases.setAttribute("style","width: 25%")
+        stat_active_cases.setAttribute("aria-valuenow","25")
+        stat_active_cases.setAttribute("aria-valuemin","0")
+        stat_active_cases.setAttribute("aria-valuemax","100")
+        stat_active_cases.textContent = allState[i].active_cases
+        statControl .appendChild(stat_active_cases)
+
+
+
+
+    }
 }
